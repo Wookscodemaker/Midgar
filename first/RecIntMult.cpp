1569809eaa525f850312aa2a789ce303e3a916ec
wooks luka <13028098282@163.com>
2025-04-01 23:58:32 +0800

    complete

--- first/RecIntMult.cpp
+++ first/RecIntMult.cpp
@@ -1,42 +1,32 @@
 //下面的算法叫做递归算法，及先对两个数进行拆分在求乘积//
 #include<iostream>
-#include<string>
+#include<math.h>
 using namespace std;
-int a[1005],b[1005],c[1005];
-string mult(string sa,string sb){
-    int lena=sa.size(),lenb=sb.size();
-    for(int i=0;i<lena;i++){
-        a[lena-1-i]=sa[i]-'0';
-    }
-    for(int i=0;i<lenb;i++){
-        b[lenb-1-i]=sb[i]-'0';
-    }
-    int lff=lena+lenb;
-    for(int i=0;i<=lena-1;i++){
-        for(int j=0;j<=lenb-1;j++){
-            c[i+j]=c[i+j]+(a[i]*b[j]);
-        }
-    }
-    for(int i=0;i<=lff;i++){
-        c[i+1]+=c[i]/10;
-        c[i]%=10;
-    }
-    string ff;
-    int z;
-    for(z=lff-1;z>=0;z--){
-        if(c[z]!=0){
-            break;
-        }
-    }
-    lff=z+1;
-    for(int i=lff-1;i>=0;i--){
-        ff+=c[i]+'0';
-    }
-    return ff;
-}
 int main (){
-    string sa,sb;
-    cin >> sa >> sb;
-    cout << mult(sa,sb) <<endl;
+    int a,b,c,n=0,m=0,x,y,k=0,q,w,e,r;
+    cin >> a >> b;
+    x=a;
+    do{
+        a=a/10;
+        n++;
+    }while(a!=0);
+    a=x;
+    y=b;
+    do{
+        b=b/10;
+        m++;
+    }while(b!=0);
+    b=y;
+    if (n%2!=0){
+        k+=1;
+        a=10*a;
+    }
+    if (m%2!=0){
+        k+=1;
+        b=10*b;
+    }
+    q=a/pow(10,n/2);w=a%(int)(pow(10,n/2));e=b/pow(10,m/2);r=b%(int)(pow(10,m/2));
+    c=((int)(pow(10,((n/2)+(m/2)))*q*e)+(int)(pow(10,n/2)*q*r)+(int)(pow(10,m/2)*w*e)+(w*r))/pow(10,k);
+    cout << c;
     return 0;
 }
