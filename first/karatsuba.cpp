a964cea1c2220dbbb1bf0eb1f41916c5512f5170
wooks luka <13028098282@163.com>
2025-04-02 23:03:17 +0800

    添加了karatsubasuanfa

---
+++ first/karatsuba.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string KaratdubaMult(string a,string b){
+    int n =a.size();
+    int m =b.size();
+    
+    return ;
+}
+int main(){
+
+}
