85d2367f64a4901e99bb6e2cde5cb1176711443c
wooks luka <13028098282@163.com>
2025-04-12 22:27:56 +0800

    compare complete

--- first/compare.cpp
+++ first/compare.cpp
@@ -5,26 +5,35 @@ int compare(int sa[1001],int sb[1001],int lena,int lenb){
     if(lena>lenb){
         return 1;
     }else if(lena<lenb){
-        return 0;
+        return -1;
     }else if(lena==lenb){
-        for(int i=0;i<lena;i++){
+        for(int i=lena-1;i>=0;i--){
             if(sa[i]!=sb[i]){
                 if(sa[i]>sb[i]){
                     return 1;
                 }else{
-                    return 0;
+                    return -1;
 
                 }
             }
         }
-        return -1;
+        return 0;
     }
-    return -1;
+    return 0;
 }
 int main(){
-    int a[1005],b[1005],m,n;
-    
-    s = compare(a[],b[],i,j);
+    int a[1005],b[1005],i,j;
+    int s,l,k;
+    string sa,sb;
+    cin >> sa >>sb;
+    i=sa.size(),j=sb.size();
+    for(l=0;l<i;l++){
+        a[i-1-l]=sa[l]-'0';
+    }
+    for(k=0;k<j;k++){
+        b[j-1-k]=sb[k]-'0'; 
+    }
+    s = compare(a,b,i,j);
     cout << s;
     return 0;
 }
