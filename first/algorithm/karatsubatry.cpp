34b2ce7b55ed85832512ec1f9f98662db56a141a
wooks luka <13028098282@163.com>
2025-08-20 22:19:45 +0800

    learn

---
+++ first/algorithm/karatsubatry.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+namespace algorithm{
+namespace karatsuba{
+/// @brief 简单实现karatsuba算法
+/// @param a 使用的两个乘积
+/// @param b 
+/// @return 乘法结果
+int karatsuba_mult(int a,int b){
+    int n,p,q;
+    p=sizeof a,q=sizeof b;
+    
+
+}
+}
+}
---
+++ first/algorithm/paper.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int i,j;
+    i=31415926;
+    string l=to_string(i);
+    j=l.length();
+    cout << j;
+    return 0;
+}
