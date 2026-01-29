3ce61d3beea20082aa4019c06670116ba7574b0a
wooks luka <13028098282@163.com>
2026-01-29 22:45:10 +0800

    add hash

--- first/test/paper.cpp
+++ first/test/paper.cpp
@@ -1,35 +1,13 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 int main(){
-    /*vector<int> a;
-    int n;
-    for (int i = 0; i < 3; i++)
-    {
-        cin >> n;
-        a.emplace_back(n);
-    }
-    for (int j = 0; j < 3; j++)
-    {
-        cout << a[j];
-    }*/
-    int i,j,k;
-    i=1;
-    i++;
-    j=i;
-    k=++i;
-    cout << j << "\n";
-    cout << k;
-    return 0;
+    unordered_map<string, double> def; 
+    def["apple"] = 7999;
+    def["xiaomi"] = 1999;
+    def["huawei"] = 3999;
+    auto it = def.find("xiaomi"); 
+    cout << it->second;
+    return 0; 
 }
- /*vector<int>swaps;
-        for(int i;i<(size(nums)-1);i++){
-            if (nums.at(i)>nums.at(i+1)){
-                swap(nums.at(i),nums.at(i+1));
-                swaps.push_back(i);
-            }
-        }
-        int a=0;
-        do{
-
-        }*/
