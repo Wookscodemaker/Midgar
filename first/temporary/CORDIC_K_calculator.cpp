7bac1b5179618d69b7e587d444df73cd8da35492
wooks luka <13028098282@163.com>
2026-02-04 12:42:23 +0800

    add something

---
+++ first/temporary/CORDIC_K_calculator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+double CORDIC_K_calculator(int n){
+    double res = 1.0;
+    double powera=1.0;
+    for(int i=0;i<n;i++){
+        res=res*(1/sqrt(1.0+1.0/powera));
+        powera*=4.0;
+    }
+    return res;
+}
+int main(){
+    cout << fixed << setprecision(16);
+    cout << CORDIC_K_calculator(100000000);
+    return 0;
+}
+/*result=0.6072529350088814*/
---
+++ first/test/trigonometric_function_calculater'CORDIC' .cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+double CORDIC_K_calculator(int n){
+    
+}
